1bcc58eeb0afd87484e08035a9c2ba0b5633ba96
Guoguo <i@qwq.trade>
2020-02-15 20:46:40 +0800

    修复 Python 3.8 无法使用 pip 安装的 Bug

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
+import sys
 from setuptools import setup, find_packages
+
+install_requires = [
+    'Click>=7.0',
+    'aiohttp>=3.6.2, <4.0',
+    'colorlog>=4.1.0',
+]
+
+if sys.version_info < (3, 7):
+    install_requires.append('dataclasses>=0.7')
 
 setup(
     name='livedownload',
     keywords=['LiveDownload', 'bilibili'],
     license='GNU General Public License v3',
     description='',
-    version='1.0',
+    version='1.0.1',
     packages=find_packages(),
     url='https://github.com/LiveDownload/LiveDownload',
     python_requires='>=3.6',
-    install_requires=[
-        'Click>=7.0',
-        'aiohttp>=3.6.2, <4.0',
-        'colorlog>=4.1.0',
-        'dataclasses>=0.7'
-    ],
+    install_requires=install_requires,
     entry_points={
         'console_scripts': [
             'livedownload=livedownload.main:main',
